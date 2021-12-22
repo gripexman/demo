67edb26c410d1208d4d4a63418ae44f7c0a0b24f
gripexman <0306191367@cao.thang.edu.vn>
2021-12-22 15:11:33 +0700

    upload file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var menuList =  document.getElementById("menuList");
+
+function togglemenu(){
+    if (menuList.style.display === "none") {
+        menuList.style.display = "block";
+      } else {
+        menuList.style.display = "none";
+      }
+}
